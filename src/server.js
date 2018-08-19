c6f91be10ac3963fcef5f4d334d2795e52d25e6a
DoodlePain <manuelfrulloscarapazzi@gmail.com>
2018-08-19 11:48:25 +0200

    little update

--- src/server.js
+++ src/server.js
@@ -3,22 +3,24 @@ var app = express()
 const bodyParser = require('body-parser')
 var mysql = require('mysql')
 
-var connection = mysql.createConnection({host: 'localhost', user: 'root', password: '****', database: 'Faceit'});
+var connection = mysql.createConnection({host: 'localhost', user: 'root', password: '*****', database: 'Faceit'});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/query', (req, res) => {
+    console.log("Query request from " + req.headers['x-forwarded-for']);
     connection
         .query('SELECT * FROM Server;', function (error, results, fields) {
             if (error) 
                 throw error;
-            console.log('The solution is: ', results);
             res.json({response: results})
         });
 })
 
 app.get('/deleteServer', (req, res) => {
+    console.log("Delete Server table data request");
+    
     connection
         .query('DELETE FROM `Server` WHERE tick=128;', function (error, results, fields) {
             if (error) 
@@ -29,6 +31,8 @@ app.get('/deleteServer', (req, res) => {
 })
 
 app.post('/insert', (req, res) => {
+    console.log("Random data insert into Server table request");
+    
     var query1 = "INSERT INTO Server (ip,locazione,porta,tick) VALUES ('"+req.body.ip+"','"+req.body.locazione+"',"+req.body.porta+","+req.body.tick+")"
     connection
         .query(query1, function (error, results, fields) {
